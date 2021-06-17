f01594b4c9ecbbd6af1a6415210839fc4b72895f
feng.qing <1187989069@qq.com>
2021-06-17 16:37:07 +0800

    测试

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: xx
+ * @Date: 2021-06-17 15:40:56
+ * @LastEditors: 青峰
+ * @LastEditTime: 2021-06-17 15:42:23
+ * @FilePath: /vue-press/docs/.vuepress/config.ts
+ */
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: 'en-US',
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+      logo: 'https://vuejs.org/images/logo.png',
+    },
+})
---
+++ docs/testfloder/foo.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: xx
+ * @Date: 2021-06-17 16:25:03
+ * @LastEditors: 青峰
+ * @LastEditTime: 2021-06-17 16:25:04
+ * @FilePath: /vue-press/docs/testfloder/foo.js
+ */
