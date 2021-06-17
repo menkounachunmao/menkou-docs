e570a05575cfc0f2f372688da6f738577f96753e
feng.qing <1187989069@qq.com>
2021-06-17 18:06:56 +0800

    github路径配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: xx
  * @Date: 2021-06-17 15:40:56
  * @LastEditors: 青峰
- * @LastEditTime: 2021-06-17 15:42:23
+ * @LastEditTime: 2021-06-17 18:06:42
  * @FilePath: /vue-press/docs/.vuepress/config.ts
  */
 import { defineUserConfig } from 'vuepress'
@@ -12,6 +12,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     lang: 'en-US',
     title: 'Hello VuePress',
     description: 'Just playing around',
+    base: '/menkou-docs/',
     themeConfig: {
       logo: 'https://vuejs.org/images/logo.png',
     },
