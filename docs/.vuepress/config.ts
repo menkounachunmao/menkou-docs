61307c6e5af5dae7df4fa9d5e2c8f948d45be9e8
feng.qing <1187989069@qq.com>
2021-06-18 10:06:00 +0800

    修改打包配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: xx
  * @Date: 2021-06-17 15:40:56
  * @LastEditors: 青峰
- * @LastEditTime: 2021-06-17 18:06:42
+ * @LastEditTime: 2021-06-18 10:05:38
  * @FilePath: /vue-press/docs/.vuepress/config.ts
  */
 import { defineUserConfig } from 'vuepress'
@@ -12,7 +12,6 @@ export default defineUserConfig<DefaultThemeOptions>({
     lang: 'en-US',
     title: 'Hello VuePress',
     description: 'Just playing around',
-    base: '/menkou-docs/',
     themeConfig: {
       logo: 'https://vuejs.org/images/logo.png',
     },
