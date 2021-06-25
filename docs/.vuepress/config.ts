ac7b6ade8ef28da9837209f8f2569ef7f1f7fbcf
feng.qing <1187989069@qq.com>
2021-06-25 17:16:55 +0800

    ADD-FEAT:  左侧导航修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: xx
  * @Date: 2021-06-17 15:40:56
  * @LastEditors: 青峰
- * @LastEditTime: 2021-06-25 16:47:42
+ * @LastEditTime: 2021-06-25 17:15:51
  * @FilePath: /vue-press/docs/.vuepress/config.ts
  */
 import { defineUserConfig } from 'vuepress'
@@ -116,6 +116,18 @@ const sidebar: DefaultThemeOptions = [
       {
         text: '渲染过程',
         link: '/html-doc/render.html',
+        children: [],
+      },
+    ],
+  },
+  {
+    text: 'WebSocket',
+    isGroup: true,
+    children: [
+      // SidebarItem
+      {
+        text: 'WebSocket',
+        link: '/web-socket-doc',
         children: [],
       },
     ],
