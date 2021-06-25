8208fb84e3039649952d41b52f810d34df6cf80d
feng.qing <1187989069@qq.com>
2021-06-25 15:25:30 +0800

    ADD-FEAT:  icon添加

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: xx
  * @Date: 2021-06-17 15:40:56
  * @LastEditors: 青峰
- * @LastEditTime: 2021-06-25 11:11:32
+ * @LastEditTime: 2021-06-25 15:25:11
  * @FilePath: /vue-press/docs/.vuepress/config.ts
  */
 import { defineUserConfig } from 'vuepress'
@@ -102,4 +102,5 @@ export default defineUserConfig<DefaultThemeOptions>({
         },
       ],
     },
+    head: [['link', { rel: 'icon', href: '/images/favicon.png' }]],
 })
